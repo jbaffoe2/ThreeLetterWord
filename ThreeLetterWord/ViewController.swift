0ccd84cde0a28c2e5d07172f00545b9fc1b8071e
Joseph Baffoe <joe.baffoe@gmail.com>
2021-10-26 18:00:01 -0600

    map

--- ThreeLetterWord/ViewController.swift
+++ ThreeLetterWord/ViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //Declare Variables and Outlets
+    
+    var letters = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
+    var counter = 0
+    
+    var currentLetter = "A"
+    
+    @IBOutlet weak var horizStackView: UIStackView!
+    
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    
+    @IBOutlet var wordLabels: [UILabel]!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
     }
 
+    
+    @IBAction func whenTapped(_ sender: Any)
+    {
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+        let selectedPoint = ((sender as! AnyObject).location(in: horizStackView))
+        
+        for label in wordLabels
+        {
+            if label.frame.contains(selectedPoint)
+            {
+                label.text = "\(currentLetter)"
+            }
+        }
+        
+        counter += 1
+        
+        if counter == 26
+        {
+            counter = resetCounter()
+            currentLetter = letters[counter]
+            currentLetterLabel.text = currentLetter
+            for label in self.wordLabels
+            {
+                label.text = "?"
+            }
+        }
+    }
+    
+    
+    
+    func resetCounter() -> Int
+    {
+        let alert = UIAlertController(title: "Resetting Counter", message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
 
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+        return 0
+    }
+    
+    
 }
 
